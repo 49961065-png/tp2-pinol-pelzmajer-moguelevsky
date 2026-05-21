e38c18b59bdac31fc21a4c19dfd864c5b36d10b3
Ferran Piñol <49961065@est.ort.edu.ar>
2026-05-20 22:32:18 -0300

    Update ErrorViewModel.cs

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,8 +1,1 @@
-namespace tp02_pinol_pelzmajer_moguelevsky.Models;
 
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
